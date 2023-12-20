2477be0db4507f2b4a510f8aba9702e59c0dceed
Kevin He <kevinjhe@bwrcr740-4.eecs.berkeley.edu>
2023-12-19 17:30:05 -0800

    updated baseline_sat to support cmsgen and unigen

--- src/utils/baseline_sat.py
+++ src/utils/baseline_sat.py
@@ -5,6 +5,8 @@ from pysat.solvers import Solver
 from pysat.examples.genhard import PHP
 import pycryptosat
 from threading import Timer
+import subprocess
+import re
 
 def interrupt(s):
     s.interrupt()
@@ -14,9 +16,14 @@ class BaselineSolverRunner(object):
 
     def __init__(self, cnf_problem: CNF, solver_name: str = "m22"):
         self.solver_name = solver_name
+        self.cnf = cnf_problem
         if solver_name == 'cms' or solver_name == 'cryptominisat':
             self.solver = pycryptosat.Solver()
             self.solver.add_clauses(cnf_problem.clauses)
+        elif solver_name == 'cmsgen':
+            pass
+        elif solver_name == 'unigen':
+            pass
         else:
             self.solver = Solver(
                 name=self.solver_name,
@@ -36,6 +43,42 @@ class BaselineSolverRunner(object):
             sat = solve_out[0]
             if sat:
                 solutions = [int(v) for v in solve_out[1][1:]]
+        elif self.solver_name == 'cmsgen':
+            sat = False
+            solutions = ""
+            elapsed_time = 0
+            try:
+                self.cnf.to_file("input.cnf")
+                p1 = subprocess.run("/tools/designs/kevinhe/cmsgen/build/cmsgen input.cnf --samplefile mysamples.out --samples 1000 --seed 0", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,text=True, timeout=7200)
+                # print(p1.stdout)
+                match = re.search(r'Total time: (\d+\.\d+)', p1.stdout)
+                if match:
+                    elapsed_time = float(match.group(1))
+                print(f"elapsed time {elapsed_time} s")
+                solutions = ""
+                with open('mysamples.out', 'r') as file:
+                    solutions = file.read()
+                sat = True
+            except subprocess.TimeoutExpired:
+                print("timeout") 
+
+        elif self.solver_name == 'unigen':
+            self.cnf.to_file("input.cnf")
+            sat = False
+            solutions = ""
+            elapsed_time = 0
+            try:
+                p1 = subprocess.run("/tools/designs/kevinhe/unigen/build/unigen --input input.cnf --sampleout mysamples.out --samples 1000 --seed 0", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,text=True, timeout=7200)
+                # print(p1.stdout)
+                match = re.search(r'Time to sample: (\d+\.\d+) s', p1.stdout)
+                if match:
+                    elapsed_time = float(match.group(1))
+                print(f"elapsed time {elapsed_time} s")
+                with open('mysamples.out', 'r') as file:
+                    solutions = file.read()
+                sat = True
+            except subprocess.TimeoutExpired:
+                print("timeout")
         else:
             timer = Timer(10000, interrupt, [self.solver])
             start_time = time()
