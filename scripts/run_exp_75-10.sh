dcf734f044068bcbb4ff3521cc58d7431d2e1e1f
jmk <mkang@eecs.berkeley.edu>
2024-02-27 16:19:41 -0800

    finalize run scripts

---
+++ scripts/run_exp_75-10.sh
@@ -0,0 +1,12 @@
+OPTIMIZER=$1
+
+# assuming x8 GPUs
+# TARGET: 1K solutions
+# will run x3 momentum values for SGD
+python src/jax/run.py \
+    -d "data/75-10/*.cnf*" -l\
+    --optimizer $OPTIMIZER\
+    --lr "5e0,1e1,2e1,5e1" \
+    --num_steps "2000" \
+    --batch_size "1000" \
+    --wandb_group "report" \
