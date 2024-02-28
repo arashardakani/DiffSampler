344fd10877076c46e09029bc09fcf0a81293a420
jmk <mkang@eecs.berkeley.edu>
2024-02-27 21:51:13 -0800

    updated scripts for 90/75-10

--- scripts/run_exp_75-10.sh
+++ scripts/run_exp_75-10.sh
@@ -3,10 +3,28 @@ OPTIMIZER=$1
 # assuming x8 GPUs
 # TARGET: 1K solutions
 # will run x3 momentum values for SGD
+# python src/jax/run.py \
+#     -d "data/75-10/*.cnf*" -l\
+#     --optimizer $OPTIMIZER\
+#     --lr "5e0,1e1,2e1,5e1" \
+#     --num_steps "1000" \
+#     --batch_size "1000" \
+#     --wandb_group "report" \
+
 python src/jax/run.py \
-    -d "data/75-10/*.cnf*" -l\
-    --optimizer $OPTIMIZER\
-    --lr "5e0,1e1,2e1,5e1" \
+    -d "data/75-10/*-3-*.cnf*" -l\
+    --optimizer "adam"\
+    --lr "2.0,2.2,2.5,2.75" \
+    --b1 "0.9"\
+    --b2 "0.99"\
+    --num_steps "5000" \
+    --batch_size "5000" \
+    --wandb_group "report" \
+
+python src/jax/run.py \
+    -d "data/75-10/*-3-*.cnf*" -l\
+    --optimizer "sgd"\
+    --lr "2.0,2.2,2.5,2.75" \
     --num_steps "2000" \
-    --batch_size "1000" \
+    --batch_size "2000" \
     --wandb_group "report" \
--- scripts/run_exp_prod.sh
+++ scripts/run_exp_prod.sh
@@ -1,9 +1,9 @@
 OPTIMIZER=$1
 
 python src/jax/run.py \
-    -d "data/prod/*.cnf*" \
+    -d "data/prod/*1s*.cnf*" \
     --optimizer ${OPTIMIZER} -l \
-    --lr "1e0,5e0,8e0,1e1,1.2e2,1.5e1" \
-    --num_steps "5000,10000" \
+    --lr "1e1,1.2e2,1.5e1,2e1" \
+    --num_steps "10000" \
     --batch_size "2000,5000" \
     --wandb_group "report" \
